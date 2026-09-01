498864d7019b5ef4a3801a37d15374cb6072bcef
allusionsafk <293703832+allusionsafk@users.noreply.github.com>
2026-09-01 17:59:36 -0400

    chore: publish Adaptive Media v0.1.0 download branch

--- src/localai/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Python orchestration package for localai."""
-
-__all__ = ["__version__"]
-
-__version__ = "0.1.1"
--- src/localai/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Run the localai command line interface."""
-
-from localai.cli import app
-
-app()
